7d39fb0e8c8e97f727bcffa7832b24598f8d0af0
Kleis Auke Wolthuizen <github@kleisauke.nl>
2025-04-17 16:17:52 +0200

    Avoid subclassing the `bdist_wheel` command
    This is no longer necessary in recent versions of setuptools.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ import sys
 
 from os import path
 from setuptools import setup
-from wheel.bdist_wheel import bdist_wheel
 
 base_dir = path.dirname(__file__)
 src_dir = path.join(base_dir, 'pyvips', 'pyvips')
@@ -12,17 +11,6 @@ src_dir = path.join(base_dir, 'pyvips', 'pyvips')
 sys.path.insert(0, src_dir)
 
 
-class bdist_wheel_abi3(bdist_wheel):
-    def get_tag(self):
-        python, abi, plat = super().get_tag()
-
-        if python.startswith('cp'):
-            # on CPython, our wheels are abi3 and compatible back to 3.7
-            return 'cp37', 'abi3', plat
-
-        return python, abi, plat
-
-
 if 'bdist_wheel' in sys.argv:
     cffi_modules = ['pyvips/pyvips/pyvips_build.py:ffibuilder']
 else:
@@ -30,5 +18,5 @@ else:
 
 setup(
     cffi_modules=cffi_modules,
-    cmdclass={'bdist_wheel': bdist_wheel_abi3},
+    options={'bdist_wheel': {'py_limited_api': f'cp{sys.version_info.major}{sys.version_info.minor}'}},
 )
